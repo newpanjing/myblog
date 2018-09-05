af6e5a2a423a302f362e3badc3b848307ec2074b
newpanjing <newpanjing@163.com>
2018-09-05 18:13:25 +0800

    后台主题改为simpleui

--- myblog/settings.py
+++ myblog/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = ['*', ]
 # Application definition
 
 INSTALLED_APPS = [
-    'suit',
+    # 'suit',
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
